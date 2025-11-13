b512da57aa153c889c13781382225587d20658aa
UtkarshU <umapus@iitbhilai.ac.in>
2025-11-13 23:47:33 +0530

    Create the Model for Posts

---
+++ backend/models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  caption: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
